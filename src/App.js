b2c16bc74544d06fa51fade28f6fb2160973cafa
jobyktom <jobyktom@gmail.com>
2020-08-03 22:31:08 +0100

    updating app with collection-preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
-import { Switch, Route, Link } from "react-router-dom";
-const Hats = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats ID :  </h1>
-      <Link to="/">Home</Link>
-      <Link to={`${props.match.url}/13`}>To 13</Link>
-      <button onClick={()=> props.history.push('/')}>Home</button>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shop.component'
+import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
